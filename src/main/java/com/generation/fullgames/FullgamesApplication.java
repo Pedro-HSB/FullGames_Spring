512fafe873048b7bdcfa740780860cbef5b6eacb
pedro-hsb <pedro.bernardo4561@gmail.com>
2023-12-14 15:47:09 -0300

    banco e projeto

---
+++ src/main/java/com/generation/fullgames/FullgamesApplication.java
@@ -0,0 +1,13 @@
+package com.generation.fullgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/fullgames/FullgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.fullgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
